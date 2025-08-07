e3055f39c2d5a348725002a60f887c1445559efa
AnilkumarFullStackDevTech <anilkumarmogili9@gmail.com>
2025-08-07 13:31:11 +0530

    Check number is even or odd in NUnitTests

---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            int result = calc.AddNumber(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void IsOddChecker_InputEveNumber_ReturnFalse()
+        {
+            //Arrnge
+            Calculator calc = new();
+
+            //Act
+            bool isOdd = calc.IsOddNumber(10);
+
+            //Assert
+            Assert.That(isOdd, Is.EqualTo(false));
+            Assert.IsFalse(isOdd);
+        }
+
+        [Test]
+        public void IsOddChecker_InputOddNumber_ReturnTrue()
+        {
+            //Arrange
+            Calculator calc = new();
+            //Act
+            bool isOdd = calc.IsOddNumber(11);
+            //Assert
+            Assert.That(isOdd, Is.EqualTo(true));
+            Assert.IsTrue(isOdd);
+        }
+    }
+}
